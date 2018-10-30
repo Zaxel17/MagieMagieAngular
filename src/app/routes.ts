e1f73989423839cf2da69880785264d6b66d6cb7
Your Name <you@example.com>
2018-10-30 11:33:37 +0100

    Ajout images

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,8 @@
 import {Route} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {GameComponent} from './game/game.component';
 
 export const routes:Route[] = [
   {path:'', component:HomeComponent}
+  {path:'game', component:GameComponent}
 ]
